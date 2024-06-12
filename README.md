696a8395df914b4c40a471c25e48744dc1d38e72
Tanakrit-D <127579510+Tanakrit-D@users.noreply.github.com>
2024-06-12 19:13:20 +0700

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,39 @@
 # Enhanced Mobile Scroll Library
  
+## Work in Progress
+
+The goal of this library is to provide more robust and useful scrolling functionality for Appium mobile automation.
+
+We achieve this by dividing the viewport/screen into 'scrollable' regions, and then performing w3c actions within this space.
+Using percentages of the viewport and element locations, we can avoid the standard approach of specifiying co-ordinates for each swipe/or scroll action.
+
+The current task is writing direction detection into the functionality. This will remove the use for specifying it manually via. parameter.
+
+Will eventually publish it as a library :^)
+
+Approach sample:
+```
+def __init__(self, driver, **kwargs):
+    super(EnhancedScroll, self).__init__()
+    self.driver = driver
+
+    # Set viewport dimensions
+    self.view_port_width, self.view_port_height = self.retrieve_viewport_dimensions
+    self.view_port_x_midpoint, self.view_port_y_midpoint = self.view_port_width // 2, self.view_port_height // 2
+    
+    # Default crop factor percentage bounds of the viewport for the scrollable area
+    self.upper_cf = kwargs.get('upper_cf', 0.05)  # 5% Removed from top of viewport
+    self.lower_cf = kwargs.get('lower_cf', 0.80)  # 20% Removed from bottom of viewport
+    self.left_cf = kwargs.get('left_cf', 0.05)    # 5% Removed from left of viewport
+    self.right_cf = kwargs.get('right_cf', 0.95)  # 5% Removed from right of viewport
+    
+    # Set scrolling boundries
+    self.upper_bound = self.view_port_height * self.upper_cf
+    self.lower_bound = self.view_port_height * self.lower_cf
+    self.left_bound = self.view_port_width * self.left_cf
+    self.right_bound = self.view_port_width * self.right_cf
+
+    # Set scrollable area
+    self.scrollable_x = (self.view_port_height - self.upper_bound) - (self.view_port_height - self.lower_bound)
+    self.scrollable_y = (self.view_port_width - self.left_bound) - (self.view_port_width - self.right_bound)
+```
--- src/enhanced_scroll/enhanced_scroll.py
+++ src/enhanced_scroll/enhanced_scroll.py
@@ -18,10 +18,18 @@ from selenium.common.exceptions import (
 
 @unique
 class Direction(Enum):
-    DOWN = "down"
     UP = "up"
-    RIGHT = "right"
+    DOWN = "down"
     LEFT = "left"
+    RIGHT = "right"
+
+class Direction_X(Enum):
+    LEFT = "left"
+    RIGHT = "right"
+
+class Direction_Y(Enum):
+    UP = "up"
+    DOWN = "down"
 
 class EnhancedScroll(object):
     """
@@ -74,7 +82,7 @@ class EnhancedScroll(object):
         """
         action = ActionChains(self.driver)
         action.w3c_actions = ActionBuilder(self.driver, mouse=PointerInput(Interaction.POINTER_TOUCHER, "touch"))
-        self.perform_navigation_full(action, self.view_port_x_midpoint, self.lower_bound, self.upper_bound)
+        self.perform_navigation_full(action, self.lower_bound, self.upper_bound)
     
     def swipe_down(self):
         """
@@ -82,7 +90,7 @@ class EnhancedScroll(object):
         """
         action = ActionChains(self.driver)
         action.w3c_actions = ActionBuilder(self.driver, mouse=PointerInput(Interaction.POINTER_TOUCHER, "touch"))
-        self.perform_navigation_full(action, self.view_port_x_midpoint, self.upper_bound, self.lower_bound)
+        self.perform_navigation_full(action, self.upper_bound, self.lower_bound)
     
     def swipe_up_on_element(self, locator_method: AppiumBy, locator_value: str):
         """
@@ -91,55 +99,93 @@ class EnhancedScroll(object):
         # Need to use crop factors on the element itself based on size + location
         print() 
             
-    def swipe_element_into_view(self, locator_method: AppiumBy, locator_value: str, direction: Direction):
+    def swipe_element_into_view(self, locator_method: AppiumBy, locator_value: str):
         """"
         DocString
         """
-        # Need to write function to determine direction of element based on location to avoid having to specify direction as parameter
         action = ActionChains(self.driver)
         action.w3c_actions = ActionBuilder(self.driver, mouse=PointerInput(Interaction.POINTER_TOUCHER, "touch"))
         element_x, element_y = self.retrieve_element_location(locator_method, locator_value)
+        direction_x, direction_y = self.retrieve_relative_direction(element_x, element_y)
 
-        if direction in [Direction.UP, Direction.DOWN]:
+        if direction_y in [Direction.UP, Direction.DOWN]:
             distance_to_element = element_y - self.lower_bound
             actions_total = distance_to_element / self.scrollable_x
             actions_complete = distance_to_element // self.scrollable_x
             actions_partial = self.scrollable_x * (actions_total - int(actions_total))
             actions_complete = int(actions_complete)
 
+            if direction_y == Direction.DOWN:
+                if actions_total > 1:
+                    self.perform_navigation_full(action, self.lower_bound, self.upper_bound, actions_complete)
+                if actions_partial > 0:
+                    self.perform_navigation_partial(action, self.lower_bound, self.upper_bound)
+            
+            if direction_y == Direction.UP:
+                if actions_total > 1:
+                    self.perform_navigation_full(action, self.upper_bound, self.lower_bound, actions_complete)
+                if actions_partial > 0:
+                    self.perform_navigation_partial(action, self.upper_bound, self.lower_bound)
 
-        if direction in [Direction.LEFT, Direction.RIGHT]:
+        if direction_x in [Direction.LEFT, Direction.RIGHT]:
             distance_to_element = element_x - self.lower_bound
             actions_total = distance_to_element / self.scrollable_y
             actions_complete = distance_to_element // self.scrollable_y
             actions_partial = self.scrollable_y * (actions_total - int(actions_total))
-            actions_complete = int(actions_complete)        
+            actions_complete = int(actions_complete)
+        
+        if actions_total > 1:
+            self.perform_navigation_full(action, self.lower_bound, self.upper_bound, actions_complete)
+        if actions_partial > 0:
+            self.perform_navigation_partial
 
-    def experimental(self, locator_method: AppiumBy, locator_value: str):
-        element_x, element_x = self.retrieve_element_location(locator_method, locator_value)
-        if element_x > self.view_port_x_midpoint:
-            direction = Direction.RIGHT
+    def retrieve_relative_direction(self, element_x, element_y):
+        # Is element outside of viewport?
+        outside_x = False
+        outside_y = False
+        element_relative_x = element_x - self.view_port_x_midpoint
+        element_relative_y = element_y - self.view_port_y_midpoint
+
+        if (element_relative_x  < 0 or
+        element_relative_x > self.view_port_width):
+            outside_x = True
+        
+        if (element_relative_y  < 0 or
+        element_relative_y > self.view_port_height):
+            outside_y = True       
+
+        
+        #######
+
         if element_x < self.view_port_x_midpoint:
-            direction = Direction.LEFT
+            direction_x = Direction_X.LEFT
+        if element_x > self.view_port_x_midpoint:
+            direction_x = Direction_X.RIGHT
+        if element_y < self.view_port_y_midpoint:
+            direction_y = Direction_Y.UP
+        if element_y > self.view_port_y_midpoint:
+            direction_y = Direction_Y.DOWN
+        return direction_x, direction_y
 
-    def perform_navigation_full(self, action: ActionChains, view_port_x_midpoint: int, initial_bound: int, finish_bound: int):
+    def perform_navigation_full(self, action: ActionChains, initial_bound: int, finish_bound: int, iterations: int = 1):
         """
         DocString
         """
-        action.w3c_actions.pointer_action.move_to_location(view_port_x_midpoint, initial_bound)
-        action.w3c_actions.pointer_action.pointer_down()
-        action.w3c_actions.pointer_action.move_to_location(view_port_x_midpoint, finish_bound)
-        action.w3c_actions.pointer_action.pause(1)
-        action.w3c_actions.pointer_action.release()
-        action.perform()
+        for i in range(iterations):
+            action.w3c_actions.pointer_action.move_to_location(self.view_port_x_midpoint, initial_bound)
+            action.w3c_actions.pointer_action.pointer_down()
+            action.w3c_actions.pointer_action.move_to_location(self.view_port_x_midpoint, finish_bound)
+            action.w3c_actions.pointer_action.pause(1)
+            action.w3c_actions.pointer_action.release()
+            action.perform()
 
-    def perform_navigation_partial(self, action: ActionChains, view_port_midpoint: int, initial_bound: int, partial_perecentage: int):
+    def perform_navigation_partial(self, action: ActionChains, initial_bound: int, partial_perecentage: int):
         """
         DocString
         """
-        action.w3c_actions.pointer_action.move_to_location(view_port_midpoint, initial_bound)
+        action.w3c_actions.pointer_action.move_to_location(self.view_port_midpoint, initial_bound)
         action.w3c_actions.pointer_action.pointer_down()
-        action.w3c_actions.pointer_action.move_to_location(view_port_midpoint, initial_bound + partial_perecentage)
+        action.w3c_actions.pointer_action.move_to_location(self.view_port_midpoint, initial_bound + partial_perecentage)
         action.w3c_actions.pointer_action.pause(1)
         action.w3c_actions.pointer_action.release()
         action.perform()
